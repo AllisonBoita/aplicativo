e8789ee87ca375f23962ff00835fc60c063d7f53
Allison Cristiano Boita <61565817+AllisonBoita@users.noreply.github.com>
2024-10-15 11:24:45 -0300

    Aula 03: App de Sorteio

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +13,11 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun clique(view: View) {
-        println("Clicado")
+    fun sortear(view: View) {
+        val textoResultado = findViewById<TextView>(R.id.text_resultado) // encontre um item de interface pelo ID
+        val numeroSorteado = Random().nextInt(100) //selecione numeros de 0 a 99 - o 100 não está incluso neste caso
+        textoResultado.setText("Número Gerado: $numeroSorteado") // configure um texto no meu text_view.
+
     }
 
 }
